3401cf7f738d8da14066a53f10bea4b3c425688d
abeerfedo <adamabeer44@gmail.com>
2025-07-10 02:55:27 +0300

    Write the logic to get the human choice
    by creating getHumanChoice function

--- javascrips.js
+++ javascrips.js
@@ -1,23 +1,24 @@
 
 
-function getComputerChoice(){
+function getComputerChoice() {
 
     let random = Math.random()
- if (random <  0.33) {
-    return "Rock";
- }
-  if (random < 0.6) {
-    return "Scissors";
+    if (random < 0.33) {
+        return "Rock";
+    }
+    if (random < 0.6) {
+        return "Scissors";
 
- }
+    }
     return "Paper";
- 
+
 }
 
+function getHumanChoice() {
 
-function getHumanChoice(){
 
-    
 }
 
-console.log(getComputerChoice())
+let human = prompt("enter rock or paper or Scissors");
+
+console.log(human)
